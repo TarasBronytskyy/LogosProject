990b2cdf193760650e7bb8781aa01ba3b7538110
Taras <mumume4e4ka@gmail.com>
2015-09-01 14:51:23 +0300

    Попробував створити кістяк

---
+++ src/main/java/employees/Employee.java
@@ -0,0 +1,17 @@
+package employees;
+
+public class Employee {
+
+	private String name;
+	private int age;
+	private String gender;
+	private String login;
+	private String password;
+	private String position;
+	private double salary;
+	
+	
+	
+	
+	
+}
